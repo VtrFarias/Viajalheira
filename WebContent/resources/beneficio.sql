16bc16f6f49df4c9444ccc84c0209448498c3437
valdemirlsf <valdemirlsf@gmail.com>
2019-02-12 21:16:04 -0300

    Alterações realizadas na tela de incluir hospedagem

---
+++ WebContent/resources/beneficio.sql
@@ -0,0 +1,7 @@
+INSERT INTO `viajalheira`.`beneficio` (`descricao`) VALUES ('Café da manhã');
+INSERT INTO `viajalheira`.`beneficio` (`descricao`) VALUES ('Almoço');
+INSERT INTO `viajalheira`.`beneficio` (`descricao`) VALUES ('Jantar');
+INSERT INTO `viajalheira`.`beneficio` (`descricao`) VALUES ('Acesso a piscina');
+INSERT INTO `viajalheira`.`beneficio` (`descricao`) VALUES ('Dormitorio compartilhado');
+INSERT INTO `viajalheira`.`beneficio` (`descricao`) VALUES ('Dormitório individual');
+
